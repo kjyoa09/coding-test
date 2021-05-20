faeb375e06473668e229a6bb9c4352cbe4b0f4ca
Donghyun <kjyoa09@naver.com>
2021-05-20 18:03:01 +0900

    17626

--- bj/class 3/17626.py
+++ bj/class 3/17626.py
@@ -1,4 +1,19 @@
 import sys
+from math import ceil, sqrt
+sys.stdin = open("C:\\Users\\LYJ_lab\\Desktop\\동현\\coding-test\\bj\\class 3\\in.txt","r")
 
-sys.stdin = open("in.txt")
-print(sys.stdin.readline())
+n = int(sys.stdin.readline())
+
+inf = float("INF")
+arr = [0,1]
+for i in range(2,n+1):
+    cnt = inf
+    if i == ceil(sqrt(i))**2:
+        arr += [1]
+    else:
+        for num in range(1,ceil(sqrt(i))):
+            tmp = i - num**2
+            if cnt > arr[tmp] + 1:
+                cnt = arr[tmp] + 1
+        arr += [cnt]
+print(arr[-1])
