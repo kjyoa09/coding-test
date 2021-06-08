d37e263b406dc2be34125f1b87eeb81d6079b902
Donghyun <kjyoa09@naver.com>
2021-06-08 23:03:06 +0900

    0601

--- pg/level 1/완주하지 못한 선수.py
+++
@@ -1,4 +0,0 @@
-from collections import Counter
-def solution(participant, completion):
-    ans = Counter(participant) - Counter(completion)
-    return list(ans.keys())[0]
--- pg/level 1/폰켓몬.py
+++
@@ -1,7 +0,0 @@
-def solution(nums):
-    sp = len(set(nums))
-    
-    if sp < len(nums)//2:
-        return sp
-    else:
-        return len(nums)//2
