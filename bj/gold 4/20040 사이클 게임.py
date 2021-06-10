1e5710f28611605d58c541e337ea74b75c8014e9
Donghyun <kjyoa09@naver.com>
2021-06-10 17:57:44 +0900

    20040 사이클 게임

---
+++ bj/gold 4/20040 사이클 게임.py
@@ -0,0 +1,24 @@
+from sys import stdin
+stdin = open("in.txt")
+N,M = map(int,stdin.readline().rstrip().split())
+
+def find(idx):
+    if arr[idx] != idx:
+        arr[idx] = find(arr[idx])
+    return arr[idx]
+def union(idx1,idx2):
+    ro1 = arr[idx1];ro2 = arr[idx2]
+    arr[max(ro1,ro2)] = min(ro1,ro2)
+    find(min(ro2,ro1))
+arr = list(range(N))
+
+for i in range(M):
+    a,b = map(int,stdin.readline().rstrip().split())
+    if find(a) != find(b):
+        union(a,b)
+    else:
+        print(i+1)
+        break
+else:
+    print(0)
+print(arr)
