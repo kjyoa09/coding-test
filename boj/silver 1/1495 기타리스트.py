64ebaf1ebae87e279b5ec4c6dc2d0809c8f4bc6b
Donghyun <kjyoa09@naver.com>
2021-06-16 17:27:58 +0900

    1495 기타리스트

---
+++ boj/silver 1/1495 기타리스트.py
@@ -0,0 +1,21 @@
+from sys import stdin
+stdin = open("in.txt","r")
+N,S,M = map(int,stdin.readline().rstrip().split())
+arr = list(map(int,stdin.readline().rstrip().split()))
+tmp = [-1]*(M+1)
+tmp[S] = S
+for a in arr:
+    re = [-1]*(M+1)
+    for t in tmp :
+        if t != -1:
+            if t-a >=0:
+                re[t-a] = t-a
+            if t+a <= M:
+                re[t+a] = t+a
+    tmp = re
+for t in tmp[::-1]:
+    if t!=-1:
+        print(t)
+        break
+else:
+    print(-1)
