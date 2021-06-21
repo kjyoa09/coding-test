7b29948a55c81fac3a1be99c80bd7f9542c5c696
Donghyun <kjyoa09@naver.com>
2021-06-21 14:45:27 +0900

    2651 자동차경주대회

---
+++ boj/gold 5/2651 자동차경주대회.py
@@ -0,0 +1,71 @@
+from sys import stdin
+stdin = open("in.txt","r")
+input = stdin.readline
+N = int(input());M = int(input())
+
+dis = list(map(int,input().rstrip().split()))
+cost = list(map(int,input().rstrip().split()))
+inf = float("INF")
+maps = dict()
+check = [dis[0]]
+maps[check[0]] = [0,[]]
+
+for i in range(M):
+    tmp_ch = [dis[i+1]]
+    tmp_maps = dict()
+    tmp_maps[dis[i+1]] = [inf,[]]
+    for ch in check:
+        if ch + dis[i+1] <= N:
+            value = tmp_maps.get(ch + dis[i+1],[inf,inf])
+            if value[0] > maps[ch][0]:# 정류소 안들려도 됨
+                tmp_maps[ch + dis[i+1]] = maps[ch]
+                tmp_ch += [ch + dis[i+1]]
+        if tmp_maps[dis[i+1]][0] > maps[ch][0] + cost[i] : # 정류소 그냥 들림
+            tmp_maps[dis[i+1]] = [maps[ch][0] + cost[i],maps[ch][1] + [i+1]]
+    check = tmp_ch 
+    maps = tmp_maps
+
+ans = [inf,[]]
+for ch in check:
+    if maps[ch][0] < ans[0]:
+        ans = maps[ch]
+print(ans[0])
+print(len(ans[1]))
+for i in ans[1]:
+    print(i,end = " ")
+
+'''
+메모리 초과
+from sys import stdin
+stdin = open("in.txt","r")
+input = stdin.readline
+N = int(input());M = int(input())
+
+dis = list(map(int,input().rstrip().split()))
+cost = list(map(int,input().rstrip().split()))
+inf = float("INF")
+maps = [[inf,[]] for _ in range(N+1)]
+check = [dis[0]]
+maps[check[0]] = [0,[]]
+
+for i in range(M):
+    tmp_ch = [dis[i+1]]
+    tmp_maps = [[inf,[]] for _ in range(N+1)]
+    for ch in check:
+        if ch + dis[i+1] <= N and tmp_maps[ch + dis[i+1]][0] > maps[ch][0]: # 정류소 안들려도 됨
+            tmp_maps[ch + dis[i+1]] = maps[ch]
+            tmp_ch += [ch + dis[i+1]]
+        if tmp_maps[dis[i+1]][0] > maps[ch][0] + cost[i] : # 정류소 그냥 들림
+            tmp_maps[dis[i+1]] = [maps[ch][0] + cost[i],maps[ch][1] + [i+1]]
+    check = tmp_ch 
+    maps = tmp_maps
+
+ans = [inf,[]]
+for ch in check:
+    if maps[ch][0] < ans[0]:
+        ans = maps[ch]
+print(ans[0])
+print(len(ans[1]))
+for i in ans[1]:
+    print(i,end = " ")
+'''
