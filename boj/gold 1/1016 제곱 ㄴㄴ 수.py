11db95899613568dde9a60caacf4a65352394b49
Donghyun <kjyoa09@naver.com>
2021-06-26 19:08:17 +0900

    1016 제곱 ㄴㄴ 수

---
+++ boj/gold 1/1016 제곱 ㄴㄴ 수.py
@@ -0,0 +1,12 @@
+from sys import stdin
+stdin = open("in.txt","r")
+input = stdin.readline
+s,e = map(int,input().rstrip().split())
+ans = [True] * (e-s+1)
+f = int(e**(1/2)//1)
+for i in range(2,f+1):
+    tmp = i**2
+    tmp = (s//tmp+1) * tmp if s%tmp else s
+    for tt in range(tmp,e+1,i**2):
+        ans[tt-s] = False
+print(sum(ans))
