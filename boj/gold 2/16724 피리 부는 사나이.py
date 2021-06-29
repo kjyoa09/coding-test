4232fe5271d8d3fc916dfc079062dac930c635ba
Donghyun <kjyoa09@naver.com>
2021-06-29 16:52:23 +0900

    16724 피리 부는 사나이

--- boj/gold 2/16724 피리 부는 사나이.py
+++ boj/gold 2/16724 피리 부는 사나이.py
@@ -1,3 +1,7 @@
+# 백트래킹
+# 다음 값이 숫자면 같은 그룹
+# "a" 면 다음 값 탐색
+# "b" 면 ans += 1 해주고 찾은 값들 새로운 그룹으로 만듬
 from sys import stdin
 stdin = open("in.txt")
 input = stdin.readline
@@ -5,9 +9,7 @@ N,M = map(int,input().rstrip().split())
 maps = [list(input().rstrip()) for _ in range(N)]
 direct = {"D":(1,0),"L":(0,-1),"R":(0,1),"U":(-1,0)}
 check = [["a"]*M for _ in range(N)]
-
 ans = 0
-
 def dfs(r,c):
     global ans
     px,py = r + direct[maps[r][c]][0],c + direct[maps[r][c]][1]
@@ -22,7 +24,6 @@ def dfs(r,c):
         check[r][c] = "b"
         check[r][c] = dfs(px,py)
         return check[r][c]
-
 for r in range(N):
     for c in range(M):
         if check[r][c] == "a":
