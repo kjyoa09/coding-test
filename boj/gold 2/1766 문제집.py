e9c34dabfd5a94aebdbb7662eb46cf8fbcf516fe
Donghyun <kjyoa09@naver.com>
2021-06-29 16:06:27 +0900

    1766 문제집

---
+++ boj/gold 2/1766 문제집.py
@@ -0,0 +1,26 @@
+from sys import stdin
+import heapq as hq
+stdin = open("in.txt")
+input = stdin.readline
+N,M = map(int,input().rstrip().split())
+dg = [float("INF")]+[0]*(N)
+v = [[]for _ in range(N+1)]
+for _ in range(M):
+    a,b = map(int,input().rstrip().split())
+    v[a].append(b)
+    dg[b] += 1
+que = []
+for idx,va in enumerate(dg):
+    if va == 0:
+        hq.heappush(que,idx)
+
+ans = []
+while que:
+    tmp = hq.heappop(que)
+    ans += str(tmp)
+    for i in v[tmp]:
+        dg[i] -= 1
+        if dg[i] == 0:
+            hq.heappush(que,i)
+print(" ".join(ans))
+
