81fcd8be3a70aedc13ca0305320f9606225ab63a
Donghyun <kjyoa09@naver.com>
2021-09-10 21:45:36 +0900

    9465 스티커

--- boj/Silver II/9465 스티커.py
+++ boj/Silver II/9465 스티커.py
@@ -1,3 +1,4 @@
+# N == 1
 import sys
 
 sys.stdin = open("in.txt")
@@ -11,9 +12,13 @@ for _ in range(T):
     maps += [list(map(int,sys.stdin.readline().rstrip().split()))]
     maps += [list(map(int,sys.stdin.readline().rstrip().split()))]
 
-    start = [max(maps[0][0],maps[1][0]),maps[1][0] + maps[0][1] ,maps[0][0] + maps[1][1]]
+    if n == 1:
+        print(max(maps[0][0],maps[1][0]))
+    else:
+        start = [max(maps[0][0],maps[1][0]),maps[1][0] + maps[0][1] ,maps[0][0] + maps[1][1]]
 
-    for i in range(2,n):
-        r1,r2 = maps[0][i],maps[1][i]
-        start = [max(start),max(start[2],start[0]) + r1, max(start[1],start[0]) + r2]
-    print(max(start))
+        for i in range(2,n):
+            start = [max(start),
+            max(start[2],start[0]) + maps[0][i],
+            max(start[1],start[0]) + maps[1][i]]
+        print(max(start))
