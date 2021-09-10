350928271bf1916f80cf52837066677407133a47
Donghyun <kjyoa09@naver.com>
2021-09-10 14:10:09 +0900

    1007 백터 매칭

--- boj/Gold II/1007 백터 매칭.py
+++ boj/Gold II/1007 백터 매칭.py
@@ -1,3 +1,4 @@
+# https://nerogarret.tistory.com/21
 from itertools import combinations
 
 class ftn:
@@ -5,18 +6,31 @@ class ftn:
         self.dic = {}
         self.ans = float('inf')
 
+        self.T_x = 0
+        self.T_y = 0
+
     def insert(self,x,y,idx):
         self.dic[idx] = (x,y)    
-    
-    def dist(a,b):
-        return sum((x+y)**2 for x,y in zip(a,b))**(1/2)
+        self.T_x += x
+        self.T_y += y
+
+    def dist(self,arr):
+        x,y = 0,0
+
+        for a in arr:
+            x += self.dic[a][0]
+            y += self.dic[a][1]
+        tmp = ((2*x - self.T_x)**2 + (2*y - self.T_y)**2)**(1/2)
+        
+        if self.ans > tmp:
+            self.ans = tmp
+        
+
 
 
 from sys import stdin
 stdin = open('in.txt','r')
 input = stdin.readline
-
-
 
 for _ in range(int(input())):
     N = int(input())
@@ -25,6 +39,7 @@ for _ in range(int(input())):
     for idx in range(N):
         x,y = map(int,input().split(' '))
         sol.insert(x,y,idx)
-    
-    for 
-    print(list(combinations(sol.dic.keys(),2)))
+
+    for comb in combinations(sol.dic.keys(),N//2):
+        sol.dist(comb)
+    print(sol.ans)
