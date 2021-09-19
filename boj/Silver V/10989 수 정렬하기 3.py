f233bb28e58b637362bc470554ee1d1b81bf5a8a
Donghyun <kjyoa09@naver.com>
2021-09-19 16:11:16 +0900

    10989 수 정렬하기 3

--- boj/Silver V/10989 수 정렬하기 3.py
+++ boj/Silver V/10989 수 정렬하기 3.py
@@ -1,10 +1,12 @@
-import sys
-n = int(sys.stdin.readline())
+from sys import stdin,stdout
+stdin = open('in.txt',"r")
+input = stdin.readline
+
+n = int(input())
 ans = [0] * 10001
 
 for i in range(n):
-    ans[int(sys.stdin.readline())] += 1
+    ans[int(input())] += 1
 for i in range(10001):
-    if ans[i] != 0:
-        print("{}\n".format(i)*ans[i],end = "")
-    
+    for _ in range(ans[i]):
+        stdout.write(str(i)+'\n')
