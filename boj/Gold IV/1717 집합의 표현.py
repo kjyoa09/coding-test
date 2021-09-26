e0e89646e0f11392cbb0f072315e567cd85debfd
Donghyun <kjyoa09@naver.com>
2021-09-26 15:18:51 +0900

    1717 집합의 표현

---
+++ boj/Gold IV/1717 집합의 표현.py
@@ -0,0 +1,31 @@
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10**8)
+stdin = open('in.txt','r')
+input = stdin.readline
+
+n,m = map(int,input().strip().split(' '))
+arr = list(range(n+1))
+
+def find(idx):
+    if arr[idx] != idx:
+        arr[idx] = find(arr[idx])
+    return arr[idx]
+
+def union(idx1,idx2):
+    if idx1 > idx2:
+        arr[idx1] = idx2
+        find(idx1)
+    else:
+        arr[idx2] = idx1
+        find(idx2)
+
+for _ in range(m):
+    c,a,b = map(int,input().strip().split(' '))
+
+    if c:
+        if find(a) == find(b):
+            print('YES')
+        else:
+            print('NO')
+    else:
+        union(find(a),find(b))
