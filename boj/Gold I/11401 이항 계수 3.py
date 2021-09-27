083a7aea16af108b19d412120ec58eec22768c76
Donghyun <kjyoa09@naver.com>
2021-09-27 16:24:44 +0900

    11401 이항 계수 3

--- boj/Gold I/11401 이항 계수 3.py
+++ boj/Gold I/11401 이항 계수 3.py
@@ -1,3 +1,8 @@
+# https://ko.wikipedia.org/wiki/%ED%8E%98%EB%A5%B4%EB%A7%88%EC%9D%98_%EC%86%8C%EC%A0%95%EB%A6%AC
+# 페르마의 소정리
+# print(((fac(N)//fac(K))// fac(N-K)) % MOD) 이렇게 하면 안되네;;
+# 수학 어렵네;
+
 from sys import stdin
 stdin = open('in.txt','r')
 
