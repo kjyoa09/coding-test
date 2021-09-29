f7af77ccab71935136517ae9736ddd1e27829474
Donghyun <kjyoa09@naver.com>
2021-09-29 16:25:06 +0900

    2533 사회망  서비스(SNS)

--- boj/Gold III/2533 사회망 서비스(SNS).py
+++ boj/Gold III/2533 사회망 서비스(SNS).py
@@ -1,24 +1,30 @@
-from sys import stdin
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10**8)
 stdin = open("in.txt","r")
 input = stdin.readline
+
 N = int(input())
 maps = [[] for _ in range(N+1)]
+
 for _ in range(N-1):
     a,b = map(int,input().strip().split(' '))
     maps[a].append(b)
     maps[b].append(a)
 
-visited,SNS = [False] * (N+1),[[0] * (N+1) for _ in range(2)]
+tree = [[] for _ in range(N+1)]
+def MT(now,pre):
+    for ch in maps[now]:
+        if ch != pre:
+            tree[now].append(ch)
+            MT(ch,now)
+MT(1,0)
+ans = [[0,0] for _ in range(N+1)]# 0 : 현재 Node >> early 1 : Noob
 
 def dfs(now):
-    global visited,SNS
-    visited[now] = True
-
-    for ch in maps[now]:
-        if not visited[ch]:
-            dfs(now,ch)
-
-
-SNS[1][2] = 1
-
-visited
+    ans[now][0] = 1
+    for ch in tree[now]:
+        dfs(ch)
+        ans[now][1] += ans[ch][0]
+        ans[now][0] += min(ans[ch])
+dfs(1)
+print(min(ans[1]))
