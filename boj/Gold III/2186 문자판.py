40e106ad0ca5c124ac5835bc037951bcf07e9f36
Donghyun <kjyoa09@naver.com>
2021-10-03 20:14:35 +0900

    2186 문자판

--- boj/Gold III/2186 문자판.py
+++ boj/Gold III/2186 문자판.py
@@ -1,14 +1,30 @@
 from sys import stdin
-from collections import defaultdict
 stdin = open('in.txt','r')
 input = stdin.readline
 
 N,M,K = map(int,input().strip().split(' '))
 maps = [list(input().strip()) for _ in range(N)]
-dic = defaultdict(list)
+wanted = list(input().strip())
 
+L = len(wanted)
+ch = [[[0]*M for _ in range(N)] for _ in range(L)]
 for r in range(N):
     for c in range(M):
-        dic[maps[r][c]].append((r,c))
+        if maps[r][c] == wanted[0]:
+            ch[0][r][c] = 1
 
-wanted = input().strip()
+for l in range(1,L):
+    for r in range(N):
+        for c in range(M):
+            if maps[r][c] == wanted[l]:
+                for k in range(1,K+1):
+                    if 0<=r-k<N:
+                        ch[l][r][c] += ch[l-1][r-k][c]
+                    if 0<=r+k<N:
+                        ch[l][r][c] += ch[l-1][r+k][c]
+                    if 0<=c-k<M:
+                        ch[l][r][c] += ch[l-1][r][c-k]
+                    if 0<=c+k<M:
+                        ch[l][r][c] += ch[l-1][r][c+k]
+
+print(sum(sum([x for x in ch[-1]],[])))
