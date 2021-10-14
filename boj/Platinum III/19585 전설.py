8970195ebbc8f5ab1cfd268563c4895401223e79
Donghyun <kjyoa09@naver.com>
2021-10-14 14:52:56 +0900

    weekly

--- boj/Platinum III/19585 전설.py
+++ boj/Platinum III/19585 전설.py
@@ -2,86 +2,43 @@ from sys import stdin,stdout
 stdin = open('in.txt')
 input = stdin.readline
 
-from collections import defaultdict
-
-class Node:
-    def __init__(self):
-        self.child = defaultdict(Node)
-        self.word = False
-
-class Trie:
-    def __init__(self):
-        self.root = Node()
-    
-    def insert(self,word):
-        node = self.root
-
-        for char in word:
-            node = node.child[char]
-        node.word = True
-
-    def search(self,word,n):
-        node = self.root
-
-        for i in range(n):
-            if word[i] not in node.child:
-                return i if node.word else 0
-            node = node.child[word[i]]
-
-C, N = map(int, input().split())
-c_trie = Trie()
-n_trie = Trie()
-for _ in range(C):
-    c_trie.insert(input().rstrip())
-for _ in range(N):
-    n_trie.insert(input().rstrip()[::-1])
-
-for _ in range(int(input())):
-    query = input().rstrip()
-    n = len(query)
-    idx1 = c_trie.search(query,n)
-    if not idx1:
-        print('No')
-        continue
-    idx2 = n_trie.search(query[::-1], n)
-    print('Yes'if idx1+idx2==n else 'No')
-
-'''color_dic = {}
+color_dic = {'data':0}
 C,N = map(int,input().strip().split(' '))
-c = {}
 for _ in range(C):
     cur_dic = color_dic
     for c in input().strip():
         if c not in cur_dic:
-            cur_dic[c] = {}
+            cur_dic[c] = {'data':0}
         cur_dic = cur_dic[c]
     cur_dic['data'] = True
 
-team_dic = {}
-
+team_dic = {'data':0}
 for _ in range(N):
     cur_dic = team_dic
-    for n in input().strip():
+    for n in input().strip()[::-1]:
         if n not in cur_dic:
-            cur_dic[n] = {}
+            cur_dic[n] = {'data':0}
         cur_dic = cur_dic[n]
-    cur_dic['data'] = True
-
+    cur_dic['data'] = 1
 for _ in range(int(input())):
     tmp = input().strip()
     Len = len(tmp)
+    arr = [0] * Len
     cur_dic = color_dic
-    for idx in range(Len):
-        if tmp[idx] in cur_dic:
-            cur_dic = cur_dic[tmp[idx]]
+    for idx1 in range(Len):
+        if tmp[idx1] in cur_dic:
+            cur_dic = cur_dic[tmp[idx1]]
+            arr[idx1] += cur_dic['data']
         else:
-            if cur_dic.get('data',0) and tmp[idx] in team_dic:
-                cur_dic = team_dic[tmp[idx]]            
-            else:
-                stdout.write('No\n')
-                break
+            arr[idx1] += cur_dic['data']
+            break
+    cur_dic = team_dic
+    for idx2 in range(Len-1,-1,-1):
+        if tmp[idx2] in cur_dic:
+            cur_dic = cur_dic[tmp[idx2]]
+            arr[idx2] += cur_dic['data']
+        if arr[idx2] == 2:
+            print('Yes')           
+            break
     else:
-        if cur_dic.get('data',0):
-            stdout.write('Yes\n')
-        else:
-            stdout.write('No\n')'''
+        print('No')
