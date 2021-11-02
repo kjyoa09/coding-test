0703321cb57b20e2708f7e42cdbd0a0b6db80b01
Donghyun <kjyoa09@naver.com>
2021-11-02 20:04:21 +0900

    지형편집

---
+++ pg/level 4/지형편집.py
@@ -0,0 +1,32 @@
+from collections import Counter
+def solution(land, P, Q):
+    dic = Counter([y for x in land for y in x])
+    keys = sorted(dic.keys())
+    re = float('inf')
+    for idx,key in enumerate(keys):
+
+        if idx == 0:
+            ans,Pp,Qp = 0,0,0
+
+            for k,v in dic.items():
+
+                if k == key:
+                    Np = v
+
+                else:
+                    ans += (k-key) * v * Q 
+                    Qp += v
+
+        else:
+            Pp += Np
+            Np = dic[key]
+            Qp -= dic[key]
+
+            ans += (keys[idx-1]-key) * Np * Q
+            ans -= (keys[idx-1]-key) * Pp * P
+            ans += (keys[idx-1]-key) * Qp * Q
+
+        if ans < re:
+            re = ans
+
+    return re
