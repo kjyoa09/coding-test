31f52e9b8e1ed04ce7604828560aaf3608210afa
dh <kjyoa09@naver.com>
2023-01-05 13:26:01 +0900

    13699 점화식

---
+++ boj/Silver IV/13699 점화식.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/13699
+
+import sys
+
+# sys.stdin = open('in.txt')
+# n = int(sys.stdin.readline())
+
+input = sys.stdin.readline
+n = int(input())
+t = [1]+[0] * 35
+
+for tt in range(1,36):
+    for i in range(tt):
+        t[tt] += t[i] * t[tt-1-i]
+
+print(t[n])
+
+
