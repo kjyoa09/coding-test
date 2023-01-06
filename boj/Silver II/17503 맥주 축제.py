312344cb1fb35df38bc721a41c496f5b080e4175
dh <kjyoa09@naver.com>
2023-01-07 00:00:48 +0900

    17503 맥주축제

--- boj/Silver II/17503 맥주 축제.py
+++ boj/Silver II/17503 맥주 축제.py
@@ -3,35 +3,37 @@
 import sys
 from heapq import *
 
-# sys.stdin = open('in.txt')
+sys.stdin = open('in.txt')
 input = sys.stdin.readline
-# n,m,k = map(int,sys.stdin.readline().rstrip().split())
 # print(n,m,k)
 
 n,m,k = map(int,input().rstrip().split())
 
 arr = []
 for i in range(k):
-    # v,c = map(int,sys.stdin.readline().rstrip().split())
     v,c = map(int,input().rstrip().split())
-    heappush(arr,(c,v)) # 도수 오름차순으로 heapq 생성
+    arr.append((c,v))
 
-res = []
-ans = 0
+def ftn(arr):
+    heapify(arr) # 도수 오름차순으로 heapq 생성
 
-while arr:
-    c,v = heappop(arr)
-    ans += v
-    
-    if ans >= m:
-        print(c)
-        break
-    
-    else :
-        heappush(res,(v,c)) # 만족도 오름차순으로 heappush
-    
-    if len(res) == n:
-        v,c = heappop(res)
-        ans -= v 
+    res = []
+    ans = 0
 
-if len(arr) == 0 :print(-1)
+    while arr:
+        c,v = heappop(arr)
+        ans += v
+        
+        if ans >= m:
+            return c
+        
+        else :
+            heappush(res,(v,c)) # 만족도 오름차순으로 heappush
+        if len(res) == n:
+            v,c = heappop(res)
+            ans -= v 
+
+    if len(arr) == 0 :
+        return -1
+    
+print(ftn(arr))
